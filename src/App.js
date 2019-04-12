bba31527d37d4870461b6ff3771ee21dc90f8736
Spencer Borg <spencerborg@Spencers-MacBook-Air.local>
2019-04-11 20:16:10 -0600

    first commit dont worry about spelling ;)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>This is all about Spencer Borg</div>
+          <div /> about self - this is a story about spencer.he was born in
+          texas up until he was 19 that was when he decided it was time to
+          venture off into the world! at this point in time he lives in utah. if
+          you want more info ask him!
+          <div />
+          <div />
+          Why to attend helio
+          <ol>
+            <li>fast pace</li>
+            <li>hands on</li>
+            <li>solid instructors</li>
+          </ol>
+          <div>
+            My Interests
+            <ul>
+              <li>Cars</li>
+              <li>computers</li>
+              <li>video games</li>
+            </ul>
+          </div>
         </header>
       </div>
     );
